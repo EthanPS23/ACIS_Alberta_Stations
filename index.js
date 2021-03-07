7154c954531f656c56f33d9b8f40da341eec7191
EthanPS23 <ethanps23@gmail.com>
2021-03-07 16:04:45 -0700

    added ability for 7 day weather and stations to be saved as geoJSON

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+//const jQuery = require('jquery');
 const fs = require('fs');
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -62,6 +63,86 @@ async function getStations(){
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Function to obtain the weather information for a given weather station for the previous 7 days.
+async function get7DayWeather(stationNumber) {
+    const browser = await puppeteer.launch({
+        // headless: false,
+        // defaultViewport: null
+    });
+
+    const page = await browser.newPage();
+
+    const url = 'https://acis.alberta.ca/acis/weather-data-viewer.jsp';
+
+    // make ckeckbox selections in order to obtain the accumulated precipitation, current precip, and air temp
+    await page.goto(url);
+    await page.click("#cb_pop_pr_b", {clickCount:1});
+    await page.click('#cb_pop_pr_a', {clickCount:1});
+    await page.click('#cb_pop_at_ave',{clickCount:1});
+    await page.select('#periodSelection',"HOURLY");
+
+    // make sure that the map is fully populated before selecting a weather station, otherwise weather station will not be selected by drop down
+    await page.waitForSelector('#map > div > div > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(4) > div:nth-child(1)');
+
+    // selects the Akimina 2 weather station from drop down
+    await page.select('#acis_stations',stationNumber);
+
+    var todaysDate = new Date();
+    //todaysDate = formatDate(todaysDate);
+
+    var last7days = new Date();
+    last7days.setDate(todaysDate.getDate()-7);
+
+    var str = formatDate(last7days);
+
+    await page.$eval('input[id=datestart]', (el, value) => el.value = value, str);
+
+    await page.click('button[title="View selected weather data in a tabular format"]');
+
+    // wait for weather table to populate
+    await page.waitForSelector('#acisDataTable');
+
+    const tableDetails = await page.evaluate(()=>{
+        const grabFromRow = (row, classname) => row
+            .querySelector(`td:nth-child(${classname})`) // grab the td
+            .innerText                                   // grab the text
+            .trim();                                      // remove spaces
+
+        // defining selector
+        const DATE_ROW_SELECTOR = '#acisDataTable > tbody > tr';
+
+        // array to store data
+        const data =[];
+
+        const dateRows = document.querySelectorAll(DATE_ROW_SELECTOR);
+
+        // looping over each date row
+        for (const tr of dateRows){
+            // Verify that the data being displayed does not contain unavailable data
+            if (grabFromRow(tr,'4') != "UNAVAILBLE") {
+                data.push({
+                    station: grabFromRow(tr,'1'),
+                    dateTime: grabFromRow(tr,'2'),
+                    AirTemp: grabFromRow(tr,'3'),
+                    precipAccum: grabFromRow(tr,'6'),
+                    precip: grabFromRow(tr,'9')
+                });
+            }
+        }
+
+        return data;
+
+    })
+
+    console.log(tableDetails);
+
+    browser.close();
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Function to obtain the weather stations co-ordinates and elevation for further showing on maps
 // TODO: While reading the GPS and ID information, it is also possible to read what elements are reported by the station.
 // this could be used to programmatically obtain the correct station data besides temp and precip only.
@@ -100,9 +181,11 @@ async function getStationGPS(){
     // make sure that the map is fully populated before selecting a weather station, otherwise weather station will not be selected by drop down
     await page.waitForSelector('#map > div > div > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(4) > div:nth-child(1)');
 
+    var jsonFeatures = [];
+
     // loop through all of the weather stations in order to get their meta-data
-    for (let index = 380; index < 400; index++) {
-    //for (let index = 1; index < stations.length; index++) {
+    // for (let index = 380; index < 400; index++) {
+    for (let index = 1; index < stations.length; index++) {
         let station = stations[index];
 
         // select from the drop down the weather station
@@ -116,6 +199,7 @@ async function getStationGPS(){
         // TODO: See about shortening and clenaing up this code to not use data -> station -> stations
         const result = await page.evaluate(()=>{
             const data = []
+
             data.push({
                 stationLatitude: document.querySelector("#meta_lat").innerText,
                 stationLongitude: document.querySelector("#meta_lon").innerText,
@@ -133,6 +217,32 @@ async function getStationGPS(){
         station.stationClimateID = result[0].stationClimateID;
         station.stationWMOID = result[0].stationWMOID;
         station.stationTransportCanID = result[0].stationTransportCanID;
+        
+        // Start creating the feature JSON to be added to the overall geoJSON of ACIS weather stations
+        var feature = {
+            id: station.stationID,
+            type: 'Feature',
+            properties: {
+                name: station.stationName,
+                type: 'weather station',
+                url: 'https://acis.alberta.ca/acis/weather-station-summary.jsp?station=' + station.stationID,
+                owner: 'ACIS',
+                stationClimateID: station.stationClimateID,
+                stationWMOID: station.stationWMOID,
+                stationTransportCanID: station.stationTransportCanID
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [
+                    result[0].stationLongitude,
+                    result[0].stationLatitude
+                ],
+                elevation: result[0].stationElevation
+            }
+        }
+
+        // add the feature json of the weather station to the list of weather stations
+        jsonFeatures.push(feature);
         
         stations[index] = station;
 
@@ -188,7 +298,20 @@ async function getStationGPS(){
         
         
         
-    }
+    } 
+
+    // Create geoJson containing all of the weather stations from features
+    var geoJson = {
+        type: 'FeatureCollection',
+        features: jsonFeatures
+    };
+
+    // WRite the geoJSON results to file
+    fs.writeFile(
+        './stationsGEO.json',
+        JSON.stringify(geoJson,null,2),
+        (err) => err ? console.error('Data not written!',err):console.log('Data written!')
+    )
 
     // write to json file the results
     fs.writeFile(
@@ -275,6 +398,25 @@ async function getWeather(){
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Function to get date
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Function to read JSON file
 function jsonReader(filePath, cb){
     fs.readFile(filePath, (err, fileData) => {
@@ -305,3 +447,4 @@ function delay(time) {
 //getWeather();
 //getStations();
 getStationGPS();
+// get7DayWeather('10540');
